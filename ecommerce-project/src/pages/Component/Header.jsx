86898e91cdc612566b6a3ecf9532fd427894c6a8
Edwin Jamsranjav <edwinjamsranjav@gmail.com>
2026-03-01 17:30:20 -0600

    get cart quantity from backend

--- ecommerce-project/src/pages/Component/Header.jsx
+++ ecommerce-project/src/pages/Component/Header.jsx
@@ -1,7 +1,13 @@
 import { Link } from 'react-router'
 import './header.css'
 
-export function Header() {
+export function Header({cartItems}) {
+
+    let totalQuantity = 0;
+    cartItems.forEach((cartItem)=>{
+        totalQuantity += cartItem.quantity;
+    })
+
     return (
         <>
             <div className="header">
@@ -30,7 +36,7 @@ export function Header() {
 
                     <Link className="cart-link header-link" to="/checkout">
                         <img className="cart-icon" src="images/icons/cart-icon.png" />
-                        <div className="cart-quantity">3</div>
+                        <div className="cart-quantity">{totalQuantity}</div>
                         <div className="cart-text">Cart</div>
                     </Link>
                 </div>
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -6,13 +6,20 @@ import './HomePage.css';
 export function HomePage() {
 
     const [products, setProducts] = useState([]);
+    const [cartItems, setCartItems] = useState([]);
 
     useEffect(() => {
         axios.get('http://localhost:3000/api/products')
             .then((response) => {
-                console.log(response.data)
+                // console.log(response.data)
                 setProducts(response.data);
-        })
+        });
+
+        axios.get('http://localhost:3000/api/cart-items')
+            .then((response) => {
+                // console.log(response.data)
+                setCartItems(response.data);
+        });
     }, []);
 
     return (
@@ -20,7 +27,7 @@ export function HomePage() {
             <title>Ecommerce Project</title>
             <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
 
-            <Header />
+            <Header cartItems={cartItems}/>
 
             <div className="home-page">
                 <div className="products-grid">
