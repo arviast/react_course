0098c79eb2e1f74cf72176417c85ab30a992c44b
Edwin Jamsranjav <edwinjamsranjav@gmail.com>
2026-03-01 15:39:22 -0600

    title, icons

--- ecommerce-project/src/pages/Checkout.jsx
+++ ecommerce-project/src/pages/Checkout.jsx
@@ -6,6 +6,7 @@ export function Checkout() {
     return (
         <>
             <title>Checkout</title>
+            <link rel="icon" type="image/svg+xml" href="cart-favicon.png" />
             <div className="checkout-header">
                 <div className="header-content">
                     <div className="checkout-header-left-section">
